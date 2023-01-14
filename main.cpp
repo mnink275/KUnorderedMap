f281b529026cab5c2d745d81ed6684f2c759cf3d
Niyaz <nmingaripov@gmail.com>
2023-01-14 20:56:09 +0300

    some fixes

--- main.cpp
+++ main.cpp
@@ -3,47 +3,34 @@
 #include <string>
 
 
-
 using namespace std::chrono;
 
 int main()
 {
-    myUnorderedMap<double, double> map1;
-    myUnorderedMap<string, string> map2;
-    // myUnorderedMap<int, int> map1;
+    myUnorderedMap<double, double> doubleMap;
+    myUnorderedMap<string, string> stringMap;
+    myUnorderedMap<int, int> intMap;
     
-   
-    time_point<high_resolution_clock> start_point, end_point;
-    start_point = high_resolution_clock::now();
-
-    /*for (double i = 1.1; i < 50.5; i++)
+    for (int i = 0; i < 700; i++)
     {
-        map1[i] = i + 100;
-    }*/
-    for (double i = 1.1; i < 50.5; i++)
-    {
-        /*map1[i] = i;*/
-        map2["string key"] = "string value";
+        intMap[i] = i;
+        doubleMap[i + 0.5] = i + 0.5;
+        stringMap["key" + to_string(i)] = "value" + to_string(i);
     }
 
-    //myUnorderedMap<double, double> map2 = move(map1);
-    //myUnorderedMap<int, int> map2 = map1;
+
+
+
+
+    // performance test
+    /*time_point<high_resolution_clock> start_point, end_point;
+    start_point = high_resolution_clock::now();
+
 
     end_point = high_resolution_clock::now();
     auto start = time_point_cast<microseconds>(start_point).time_since_epoch().count();
     auto end = time_point_cast<microseconds>(end_point).time_since_epoch().count();
-    cout << "Time taken = " << (end - start) << " microseconds" << endl;
-
-    /*string str = "Hello";
-    cout << std::hash<int>{}(5);*/
-    
-  /*  map1.print();
-    auto ptr = map1.find(2.1);
-    ptr->value_type.second = 1.1;
-    map1.print();
-    cout << map1.mapSize();*/
-    /*map_reciver.print();
-    map.print();*/
+    cout << "Time taken = " << (end - start) << " microseconds" << endl;*/
 
     return 0;
 }
