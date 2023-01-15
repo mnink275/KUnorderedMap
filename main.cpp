e7c04f4d38897b1faa65de229ddecadf0ddf2140
Niyaz <nmingaripov@gmail.com>
2023-01-15 13:29:36 +0300

    added some tests

--- main.cpp
+++ main.cpp
@@ -1,25 +1,44 @@
 #include "myUnorderedMap.h"
 #include <chrono>
 #include <string>
+#include <cassert>
 
 
 using namespace std::chrono;
 
 int main()
 {
+    myUnorderedMap<int, int> intMap;
     myUnorderedMap<double, double> doubleMap;
     myUnorderedMap<string, string> stringMap;
-    myUnorderedMap<int, int> intMap;
     
+    // operator[]
     for (int i = 0; i < 700; i++)
     {
-        intMap[i] = i;
-        doubleMap[i + 0.5] = i + 0.5;
+        intMap[i] = i + 100;
+        doubleMap[i + 0.5] = i + 100.5;
         stringMap["key" + to_string(i)] = "value" + to_string(i);
     }
 
+    // find()
+    assert(intMap.find(555)->second == 655);
+    assert(doubleMap.find(13.5)->second == 113.5);
+    assert(stringMap.find("key600")->second == "value600");
 
+    // brute_force_find()
+    assert(intMap.brute_force_find(555)->second == 655);
+    assert(doubleMap.brute_force_find(13.5)->second == 113.5);
+    assert(stringMap.brute_force_find("key600")->second == "value600");
 
+    // isEmpty()
+    assert(!intMap.isEmpty());
+    myUnorderedMap<int, int> intMapOther;
+    assert(intMapOther.isEmpty());
+    
+    // print()
+    intMap.print();
+    /*doubleMap.print();
+    stringMap.print();*/
 
 
     // performance test
