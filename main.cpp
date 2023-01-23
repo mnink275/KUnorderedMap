f599fbbb3606a770de34e00dc7d3bd40e3af520d
Niyaz <nmingaripov@gmail.com>
2023-01-23 23:42:36 +0300

    some updates

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ int main()
     cout << "load_factor: " << doubleMap.load_factor() << "\n";
 
     // iterator
-    myUnorderedMap<double, double>::Iterator it = doubleMap.begin();
+    myUnorderedMap<double, double>::iterator it = doubleMap.begin();
     for (size_t i = 0; i < 3; i++) ++it;
     cout << "it_to_begin++ 3 times: " << it->second << "\n";
 
