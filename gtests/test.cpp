cb8deb36507c3af9247643a7f8a2e165d2a21c74
Niyaz <nmingaripov@gmail.com>
2023-04-18 19:57:46 +0300

    Added google tests

---
+++ gtests/test.cpp
@@ -0,0 +1,118 @@
+#include <gtest/gtest.h>
+#include <string>
+
+#include "KUnorderedMap.h"
+
+struct KekStruct {
+    KekStruct() : ival(0), dval(0), sval("") {}
+    KekStruct(int _ival, double _dval, const std::string& _sval)
+        : ival(_ival), dval(_dval), sval(_sval) {}
+    KekStruct(KekStruct&& other) noexcept :
+        ival(other.ival), dval(other.dval), sval(other.sval) {
+
+        std::cout << "KekStruct MOVE\n";
+    }
+    KekStruct& operator=(KekStruct&& other) noexcept {
+        std::cout << "KekStruct MOVE operator=\n";
+        ival = std::move(other.ival);
+        dval = std::move(other.dval);
+        sval = std::move(other.sval);
+        return *this;
+    }
+    KekStruct(const KekStruct& other) :
+        ival(other.ival), dval(other.dval), sval(other.sval) {
+
+        std::cout << "KekStruct COPY\n";
+    }
+    KekStruct& operator=(const KekStruct& other) {
+        std::cout << "KekStruct COPY operator=\n";
+        ival = other.ival;
+        dval = other.dval;
+        sval = other.sval;
+        return *this;
+    }
+
+    friend bool operator==(const KekStruct& lhs, const KekStruct& rhs) {
+        return (lhs.ival == rhs.ival)
+            || (lhs.dval == rhs.dval)
+            || (lhs.sval == rhs.sval);
+    }
+
+    int ival;
+    double dval;
+    std::string sval;
+};
+
+struct KekHash {
+    size_t operator()(const KekStruct& some_kek_struct) const {
+        return std::hash<int>{}(some_kek_struct.ival);
+    }
+};
+
+ink::KUnorderedMap<int, int> intMap;
+ink::KUnorderedMap<double, double> doubleMap;
+ink::KUnorderedMap<std::string, std::string> stringMap;
+
+// operator[]
+TEST(UMapInitialization, SquareBracketsOperator) {
+    for (int i = -700; i < 700; ++i) {
+        intMap[i] = i + 100;
+        doubleMap[i + 0.5] = i + 100.5;
+        stringMap["key: " + std::to_string(i)] = "value: " + std::to_string(i);
+    }
+}
+
+// find()
+TEST(UMapInitialization, Finding) {
+    EXPECT_EQ(intMap.find(555)->second, 655);
+    EXPECT_EQ(intMap.find(10000), intMap.end());
+
+    EXPECT_EQ(doubleMap.find(13.5)->second, 113.5);
+    EXPECT_EQ(doubleMap.find(-10000.2523), doubleMap.end());
+
+    EXPECT_EQ(stringMap.find("key: 600")->second, "value: 600");
+    EXPECT_EQ(stringMap.find("RandomKey"), stringMap.end());
+}
+
+// erase()
+TEST(UMapInitialization, Erasing) {
+    EXPECT_EQ(doubleMap.find(13.5)->second, 113.5);
+    auto iterator = doubleMap.find(13.5);
+    doubleMap.erase(iterator);
+    EXPECT_EQ(doubleMap.find(13.5), doubleMap.end());
+    EXPECT_EQ(doubleMap.erase(13.5), 0);
+    EXPECT_EQ(doubleMap.erase(14.5), 1);
+}
+
+// isEmpty()
+TEST(UMapInitialization, isEmpty) {
+    EXPECT_TRUE(!intMap.isEmpty());
+    intMap.clear();
+    EXPECT_TRUE(intMap.isEmpty());
+    EXPECT_EQ(intMap.size(), 0);
+}
+
+// copy and move operator=()
+TEST(UMapInitialization, CopyAndMoveAssignOperator) {
+    ink::KUnorderedMap<int, int> intMapOther;
+    intMap[5] = 500;
+    intMapOther = intMap;
+    EXPECT_EQ(intMapOther[5], 500);
+
+    intMapOther.clear();
+    intMapOther = std::move(intMap);
+    EXPECT_EQ(intMapOther[5], 500);
+}
+
+// maxLoadFactor()
+TEST(UMapInitialization, HashPolicy) {
+    EXPECT_EQ(doubleMap.maxLoadFactor(), 2.0f);
+    doubleMap.maxLoadFactor(5.0f);
+    EXPECT_EQ(doubleMap.maxLoadFactor(), 5.0f);
+}
+//
+//TEST(UMapInitialization, Iterators) {
+//    EXPECT_EQ(doubleMap.maxLoadFactor(), 2.0f);
+//    doubleMap.maxLoadFactor(5.0f);
+//    EXPECT_EQ(doubleMap.maxLoadFactor(), 5.0f);
+//}
--- main.cpp
+++ main.cpp
@@ -1,116 +1,10 @@
 #include "KUnorderedMap.h"
-#include <chrono>
 #include <string>
 
-using namespace std::chrono;
 
-struct KekStruct {
-    KekStruct() : ival(0), dval(0), sval("") {}
-    KekStruct(int _ival, double _dval, const std::string& _sval)
-        : ival(_ival), dval(_dval), sval(_sval) {}
-    KekStruct(KekStruct&& kek) noexcept :
-        ival(kek.ival), dval(kek.dval), sval(kek.sval) {
-
-        std::cout << "KekStruct MOVE\n";
-    }
-    KekStruct& operator=(KekStruct&& kek) noexcept {
-        std::cout << "KekStruct MOVE operator=\n";
-        ival = std::move(kek.ival);
-        dval = std::move(kek.dval);
-        sval = std::move(kek.sval);
-        return *this;
-    }
-    KekStruct(const KekStruct& kek) :
-        ival(kek.ival), dval(kek.dval), sval(kek.sval) {
-
-        std::cout << "KekStruct COPY\n";
-    }
-    KekStruct& operator=(const KekStruct& kek) {
-        std::cout << "KekStruct COPY operator=\n";
-        ival = kek.ival;
-        dval = kek.dval;
-        sval = kek.sval;
-        return *this;
-    }
-    
-    friend bool operator==(const KekStruct& left, const KekStruct& right) {
-        return (left.ival == right.ival)
-            || (left.dval == right.dval)
-            || (left.sval == right.sval);
-    }
-
-    int ival;
-    double dval;
-    std::string sval;
-};
-
-struct KekHash {
-    size_t operator()(const KekStruct& struct_type) const {
-        return std::hash<int>{}(struct_type.ival);
-    }
-};
 
 
 int main() {
-    ink::KUnorderedMap<int, int> intMap;
-    ink::KUnorderedMap<double, double> doubleMap;
-    ink::KUnorderedMap<std::string, std::string> stringMap;
-
-    // operator[]
-    for (int i = 0; i < 700; i++) {
-        intMap[i] = i + 100;
-        doubleMap[i + 0.5] = i + 100.5;
-        stringMap["key" + std::to_string(i)] = "value" + std::to_string(i);
-    }
-
-    // find()
-    assert(intMap.find(10000) == intMap.end());
-    assert(intMap.find(555)->second == 655);
-    assert(doubleMap.find(13.5)->second == 113.5);
-    assert(stringMap.find("key600")->second == "value600");
-
-    // erase()
-    auto ptr = doubleMap.find(13.5);
-    doubleMap.erase(ptr);
-    assert(doubleMap.find(13.5) == doubleMap.end());
-    assert(doubleMap.erase(13.5) == 0);
-    assert(doubleMap.erase(14.5) == 1);
-
-    // bruteForceFind()
-    assert(intMap.bruteForceFind(555)->second == 655);
-    assert(doubleMap.bruteForceFind(13.5)->second == 113.5);
-    assert(stringMap.bruteForceFind("key600")->second == "value600");
-
-    // isEmpty()
-    assert(!intMap.isEmpty());
-    ink::KUnorderedMap<int, int> intMapOther;
-    assert(intMapOther.isEmpty());
-    
-    // print()
-    // intMap.print();
-    /*doubleMap.print();
-    stringMap.print();*/
-
-    // copy operation
-    intMapOther = intMap;
-    assert(!intMap.isEmpty());
-    assert(!intMapOther.isEmpty());
-
-    // move operation
-    intMapOther = move(intMap);
-    assert(intMap.isEmpty());
-    assert(!intMapOther.isEmpty());
-
-    // other functions
-    std::cout << "totalBucketCount: " << doubleMap.totalBucketCount() << "\n";
-    std::cout << "usedBucketCount: " << doubleMap.usedBucketCount() << "\n";
-    std::cout << "load_factor: " << doubleMap.loadFactor() << "\n";
-    assert(doubleMap.maxLoadFactor() == 2.0f);
-    doubleMap.maxLoadFactor(5.0f);
-    assert(doubleMap.maxLoadFactor() == 5.0f);
-    doubleMap.clear();
-    assert(doubleMap.isEmpty());
-
 
     // iterator and const_iterator
     ink::KUnorderedMap<int, int> iterator_test_map;
@@ -144,41 +38,33 @@ int main() {
     // (compared to assignment without rehash() calls)
     ink::KUnorderedMap<int, int> PerformanceTest;
 
-    time_point<high_resolution_clock> start_point, end_point;
-    start_point = high_resolution_clock::now();
-
     for (int i = 0; i < 7000; i++) {
         PerformanceTest[i] = i + 100;
     }
 
-    end_point = high_resolution_clock::now();
-    auto start = time_point_cast<microseconds>(start_point).time_since_epoch().count();
-    auto end = time_point_cast<microseconds>(end_point).time_since_epoch().count();
-    std::cout << "Time taken = " << (end - start) << " microseconds\n";
-    assert(PerformanceTest.loadFactor() <= 2);
-
-    // custom data
-    ink::KUnorderedMap<KekStruct, int, KekHash> CustomStructMap1;
-    ink::KUnorderedMap<int, KekStruct> CustomStructMap2;
-    for (int i = 0; i < 3; i++) {
-        KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
-        CustomStructMap1[kek] = i;
-    }
-    std::cout << "\n";
-    for (int i = 0; i < 3; i++) {
-        KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
-        CustomStructMap1[std::move(kek)] = i;
-    }
-    std::cout << "\n";
-    for (int i = 0; i < 3; i++) {
-        KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
-        CustomStructMap2[i] = kek;
-    }
-    std::cout << "\n";
-    for (int i = 0; i < 3; i++) {
-        KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
-        CustomStructMap2[i] = std::move(kek);
-    }
+    //
+    //// custom data
+    //ink::KUnorderedMap<KekStruct, int, KekHash> CustomStructMap1;
+    //ink::KUnorderedMap<int, KekStruct> CustomStructMap2;
+    //for (int i = 0; i < 3; i++) {
+    //    KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
+    //    CustomStructMap1[kek] = i;
+    //}
+    //std::cout << "\n";
+    //for (int i = 0; i < 3; i++) {
+    //    KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
+    //    CustomStructMap1[std::move(kek)] = i;
+    //}
+    //std::cout << "\n";
+    //for (int i = 0; i < 3; i++) {
+    //    KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
+    //    CustomStructMap2[i] = kek;
+    //}
+    //std::cout << "\n";
+    //for (int i = 0; i < 3; i++) {
+    //    KekStruct kek(i, i + 0.5, "GLaDOS" + std::to_string(i));
+    //    CustomStructMap2[i] = std::move(kek);
+    //}
 
     return 0;
 }
