8da6b9a3e324633a77098c53858b035e8d50c8e9
Niyaz <nmingaripov@gmail.com>
2023-05-22 16:52:37 +0300

    trash cleared

--- main.cpp
+++ main.cpp
@@ -6,14 +6,6 @@
 
 int main() {
     std::cout << "Empty main()\n";
-    struct NotIterator {
-        int first{ 0 };
-        int second{ 0 };
-    };
-    NotIterator not_iterator;
-    auto* ptr = &not_iterator;
-    std::unordered_map<int, int> m(ptr, ptr);
-    std::unordered_map<int, int> init{ {1, 10} , {2, 20}, {3, 30} };
 
     return 0;
 }
